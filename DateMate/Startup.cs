6a07703595caf56a0b73d9b7da401be30e1b93c3
Emma_ <Emma_@DESKTOP-6L9NQPQ>
2017-12-21 12:23:48 +0100

    Add project files.

---
+++ DateMate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DateMate.Startup))]
+namespace DateMate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
