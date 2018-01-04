5d8db7d63b998b9869566627d305da94a518703a
Emma_ <Emma_@DESKTOP-6L9NQPQ>
2018-01-04 09:23:40 +0100

    ordning

---
+++ DateMate/Models/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DateMate.Models
+{
+    public class HomeViewModel
+    {        public string Welcome { get; set; }
+        public string Info { get; set; }
+    }
+}
